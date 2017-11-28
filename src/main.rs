0d562e3270d50c84f04b2397d8e17f6a7044e20d
Noah Rinehart <rinehart.noah@gmail.com>
2017-11-28 17:03:27 -0500

    added html and socket support, messages now work

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,73 @@
-
 extern crate ws;
-extern crate clap;
-extern crate term;
 extern crate env_logger;
+extern crate iron;
+extern crate staticfile;
+extern crate mount;
 
-use ws::listen;
-use clap::{App, Arg};
+use std::thread;
+use std::path::Path;
+
+use ws::{listen, Message, Sender, Handler, Result, Error, CloseCode};
+use iron::Iron;
+use staticfile::Static;
+use mount::Mount;
+
+struct Server {
+    ws: Sender,
+}
+
+impl Handler for Server {
+
+    fn on_open(&mut self, shake: ws::Handshake) -> ws::Result<()> {
+        if let Some(ip_addr) = try!(shake.remote_addr()) {
+            println!("Connection opened from {}.", ip_addr)
+        } else {
+            println!("Unable to obtain client's IP address.")
+        }
+        Ok(())
+    }
+
+
+    fn on_message(&mut self, msg: Message) -> Result<()> {
+        println!("Server got message '{}'. ", msg);
+
+        // echo it back
+        self.ws.send(msg)
+    }
+
+    fn on_close(&mut self, code: CloseCode, _: &str) {
+        println!("Client disconnected with reason '{:?}'", code);
+    }
+
+    fn on_error(&mut self, err: Error) {
+        println!("Shutting down server for error: {}", err);
+        self.ws.shutdown().unwrap();
+    }
+}
+
 
 fn main() {
 
     // Setup logging
     env_logger::init().unwrap();
 
-    // Setup cli args
-    let matches = App::new("Sonic")
-        .version("0.1")
-        .author("Noah Rinehart <nrinehar@purdue.edu>, Sanjeet Suhag <suhag@purdue.edu>")
-        .about("An encrypted websocket chat app.")
-        .arg(Arg::with_name("port")
-                .help("The port of the server")
-                .required(false)
-                .index(1))
-        .get_matches();
+    // Setup HTML server
+    let mut mount = Mount::new();
+    mount.mount("/", Static::new(Path::new("public/index.html")));
+    let html_server = thread::spawn(move || {
+        Iron::new(mount).http("127.0.0.1:3000").unwrap();
+    });
+    
+    // Setup WebSocket server
+    let ws_ip = "127.0.0.1:3021";
+    let ws_server = thread::spawn(move || {
+        listen(ws_ip, |out| {
+            Server { ws: out }
+        }).unwrap()
+    });
 
-    let port: String = matches.value_of("port").unwrap_or("3021").to_string();
-    let ip: String = "127.0.0.1:".to_string() + &port;
 
-    println!("Starting server on {}", port);
-    if let Err(error) = listen(ip, |out| {
-        move |msg| {
-            println!("Server recieved: '{}'", msg);
-            out.send(msg)
-        }
-    }) {
-        println!("Failed to create WebSocket. Error: {:?}", error);
-    } 
+    let _ = html_server.join();
+    let _ = ws_server.join();
 }
 
